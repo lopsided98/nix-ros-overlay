ca37dee087913d3e63aef83beed53ecdcf09cc0f
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-03-21 00:26:15 -0400

    Remove deps that aren't needed right now.

--- base.nix
+++ base.nix
@@ -23,44 +23,5 @@ in {
   python36 = pythonOverridesFor super.python36;
   python37 = pythonOverridesFor super.python37;
 
-  vcstools = buildPythonPackage rec {
-    pname = "vcstools";
-    version = "0.1.40";
-    name = "${pname}-${version}";
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "1mfasip71ky1g968n1zlramgn3fjxk4c922d0x9cs0nwm2snln4m";
-    };
-
-    propagatedBuildInputs = [ pyyaml python-dateutil ];
-
-    # Tries to download files and lots of other issues
-    doCheck = false;
-    
-    meta = {
-      description = "Python library for interacting with various VCS systems";
-      homepage = http://wiki.ros.org/vcstools;
-    };
-  };
-  
-  wstool = buildPythonPackage rec {
-    pname = "wstool";
-    version = "0.1.17";
-    name = "${pname}-${version}";
-
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "0dz2gn2qx919s1z5wa94nkvb01pnqp945mvj97108w7i1q8lz6y7";
-    };
-
-    propagatedBuildInputs = [ vcstools pyyaml ];
-
-    meta = {
-      description = "A tool for managing a workspace of multiple heterogenous SCM repositories";
-      homepage = http://wiki.ros.org/wstool;
-    };
-  };
-
   console-bridge = self.callPackage ./console-bridge { };
 }
