22fd2f38f174e7faf94bdefdd6d4d51e425c9eb5
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-04-07 15:55:30 -0400

    Better catkin env hook handling.

--- catkin-setup-hook/setup-hook.sh
+++ catkin-setup-hook/setup-hook.sh
@@ -2,27 +2,60 @@ isCatkinPackage() {
   [ -f "$1/.catkin" ]
 }
 
-declare -gA _rosPackagesSeen
+# Load catkin environment hooks. This mimics the behavior of _setup_util.py.
+
+declare -gA _catkinPackagesSeen
+declare -gA _catkinGenericEnvHooks
+declare -gA _catkinSpecificEnvHooks
+
+_findCatkinEnvHooks() {
+  local pkg="$1"
+  local pkgEnvHookDir="$pkg/etc/catkin/profile.d"
+  # Deduplicate the packages
+  if [ -z "${_catkinPackagesSeen["$pkg"]}" ] && isCatkinPackage "$pkg"; then
+    while IFS= read -rd '' hook; do
+      _catkinGenericEnvHooks["$(basename "$hook")"]="$hook"
+    done < <(find "$pkgEnvHookDir" -name "*.sh" -print0)
+    while IFS= read -rd '' hook; do
+      _catkinSpecificEnvHooks["$(basename "$hook")"]="$hook"
+    done < <(find "$pkgEnvHookDir" -name "*.$CATKIN_SHELL" -print0)
+  fi
+  _catkinPackagesSeen["$pkg"]=1
+}
+addEnvHooks "$hostOffset" _findCatkinEnvHooks
+
+_runCatkinEnvHook() {
+  [ -n "$1" ] || return 0
+  # Causes hooks to look in the wrong place
+  unset CATKIN_ENV_HOOK_WORKSPACE
+  # Some hooks fail in stripped down bash during builds
+  source "$1" || true
+}
+
+_runCatkinEnvHooksArray() {
+  # Run hooks in sorted order of their file names
+  # This would fail if a filename contained EOT
+  while IFS= read -rd '' hook; do
+    _runCatkinEnvHook "$hook"
+  done < <(printf "%s\0" "$@" | \
+           # Replace final / with EOT, separating the file name
+           sed -z 's|\(.*\)/|\1'$'\4''|' | \
+           # Sort on second EOT separated field (file name)
+           LC_ALL=C sort -zt$'\4' -k2 | \
+           # Substitute / back in for EOT
+           sed -z 's|'$'\4''|/|')
+}
 
 _runCatkinEnvHooks() {
-  local pkg="$1"
-  # Deduplicate the packages. This prevents running hooks multiple times.
-  if [ -z "${_rosPackagesSeen["$pkg"]}" ] && isCatkinPackage "$pkg"; then
-    for hook in "$pkg"/etc/catkin/profile.d/{*.sh,*.bash}; do
-      # Causes hooks to look in the wrong place
-      unset CATKIN_ENV_HOOK_WORKSPACE
-      # Some hooks fail in stripped down bash during builds
-      source "$hook" || true
-    done
-  fi
-  _rosPackagesSeen["$pkg"]=1
+  _runCatkinEnvHooksArray "${_catkinGenericEnvHooks[@]}"
+  _runCatkinEnvHooksArray "${_catkinSpecificEnvHooks[@]}"
 }
-addEnvHooks "$hostOffset" _runCatkinEnvHooks
+postHooks+=(_runCatkinEnvHooks)
 
 _catkinPostPatchHook() {
-  find . -name '*.cfg' -executable -print0 | while IFS= read -rd '' cfg; do
+  while IFS= read -rd '' cfg; do
     patchShebangs "$cfg"
-  done
+  done < <(find . -name '*.cfg' -executable -print0)
 }
 postPatchHooks+=(_catkinPostPatchHook)
 
