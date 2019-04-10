a7d774f5fc54fc1a027c429b7b686a75d9e05f1c
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-04-09 22:27:06 -0400

    lunar: add catkin patches

--- lunar/overrides.nix
+++ lunar/overrides.nix
@@ -2,4 +2,22 @@
 self:
 # Distro package set
 rosSelf: rosSuper: {
+  gazeboSimulator = self.gazeboSimulator // {
+    gazebo = self.gazeboSimulator.gazebo7;
+  };
+
+  catkin = rosSuper.catkin.overrideAttrs ({
+    patches ? [], ...
+  }: {
+    patches = patches ++ [
+      (self.fetchpatch {
+        url = https://github.com/ros/catkin/commit/98f97ffcffbb7127d4a8aff0fcb26d1578557906.patch;
+        sha256 = "1z6yl5yqrbivw1zkibkk5vn5ykrsrfmba80vmcqriiz0fdq2sigk";
+      })
+      (self.fetchpatch {
+        url = https://github.com/ros/catkin/commit/4812667954e4cb7f99a753ffd4d725047e73a73b.patch;
+        sha256 = "02i12935w119yyqvwlrd0w3z6xl786wlrkh49wsx6djaljhk3w3a";
+      })
+    ];
+  });
 }
