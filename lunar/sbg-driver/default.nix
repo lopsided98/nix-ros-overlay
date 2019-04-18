a48f6c2f6c5a5837b32971ad28db4b79bbe8d8e6
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-04-18 01:51:01 -0400

    regenerate ros-lunar, Thu Apr 18 01:51:01 2019

--- lunar/sbg-driver/default.nix
+++ lunar/sbg-driver/default.nix
@@ -8,7 +8,7 @@ buildRosPackage {
   version = "1.1.7";
 
   src = fetchurl {
-    url = https://github.com/ENSTABretagneRobotics/sbg_ros_driver-release/archive/release/lunar/sbg_driver/1.1.7-0.tar.gz;
+    url = https://github.com/SBG-Systems/sbg_ros_driver-release/archive/release/lunar/sbg_driver/1.1.7-0.tar.gz;
     sha256 = "abe0ab5b7deabd7e69d4ded311ba60f96f2b37ec1ee502c43fd4d213747f4b1b";
   };
 
