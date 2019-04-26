b337d51296aa93f64056f909b7f98bb00ecec991
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-04-26 17:23:04 -0400

    rosinstall-generator: 0.1.14 -> 0.1.16

--- rosinstall-generator/default.nix
+++ rosinstall-generator/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "rosinstall_generator";
-  version = "0.1.14";
+  version = "0.1.16";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "058wld3gcnziprsgm7c5pvdscm181nywshsfpxddyhcqr12dswjk";
+    sha256 = "15ckrcr16py0syxw5wg0yn0v6kdlr26ix57sm9qgy0zpz0ayw3i8";
   };
 
   propagatedBuildInputs = [ catkin-pkg rosdistro rospkg pyyaml ];
