4d177d8aa574b54b0dc93e5be9129b0e30a2ad3a
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-04-27 01:29:00 -0400

    Use new buildEnv implementation.
    This is kind of hacky, but it works better than the old implementation.

--- build-env/default.nix
+++ build-env/default.nix
@@ -4,14 +4,32 @@
 with lib;
 
 let
-  propagatePackages = drvs: let
-    validDrvs = filter (d: d != null) drvs;
-  in if length validDrvs == 0 then []
-    else unique (validDrvs ++ (propagatePackages (unique (concatLists (catAttrs "propagatedBuildInputs" validDrvs)))));
+  propagatePackages = packages: let
+    validPackages = filter (d: d != null) packages;
+    partitionedPackages = partition (d: d.rosPackage or false) validPackages;
+    rosPackages = partitionedPackages.right;
+    otherPackages = partitionedPackages.wrong;
+    rosPropagatedPackages = unique (concatLists (catAttrs "propagatedBuildInputs" rosPackages));
+    recurse = propagatePackages rosPropagatedPackages;
+  in if length validPackages == 0 then {
+      rosPackages = [];
+      otherPackages = [];
+    } else {
+      rosPackages = unique (rosPackages ++ recurse.rosPackages);
+      otherPackages = unique (otherPackages ++ recurse.otherPackages);
+    };
 
-  env = buildEnv (args // {
+  propagatedPaths = propagatePackages paths;
+
+  env = (buildEnv (args // {
     name = "ros-env";
-    paths = propagatePackages paths;
+    # Only add ROS packages to environment. The rest are propagated like normal.
+    # ROS packages propagate a huge number of dependencies, which are added all
+    # added to the environment with nix-shell -p, but would not normally not be
+    # added with buildEnv. This file adds all specified ROS packages and their
+    # ROS dependencies to the environment, while propagating other packages like
+    # nix-shell -p does.
+    paths = propagatedPaths.rosPackages;
     ignoreCollisions = true;
 
     passthru.env = stdenv.mkDerivation {
@@ -26,5 +44,15 @@ let
         exit 1
       '';
     };
+  })).overrideAttrs ({ buildCommand, ...}: {
+    # Hack to allow buildEnv to use propagatedBuildInputs
+    propagatedBuildInputs = propagatedPaths.otherPackages;
+    buildCommand = null;
+    oldBuildCommand = buildCommand;
+    passAsFile = [ "oldBuildCommand" ];
+    buildPhase = ''
+      . "$oldBuildCommandPath"
+    '';
+    phases = [ "buildPhase" "fixupPhase" ];
   });
 in env
