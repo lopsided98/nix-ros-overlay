27edcf1f7bb5cebc5d72cf1e6c246a0723bf7c84
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-08-30 00:25:23 -0400

    pythonPackages.catkin-pkg: 0.4.12 -> 0.4.13

--- catkin-pkg/default.nix
+++ catkin-pkg/default.nix
@@ -1,19 +1,21 @@
-{ buildPythonPackage, fetchPypi, fetchpatch, python-dateutil, docutils
+{ lib, buildPythonPackage, fetchPypi, fetchpatch, python-dateutil, docutils
 , pyparsing }:
 
 buildPythonPackage rec {
   pname = "catkin_pkg";
-  version = "0.4.12";
-  
+  version = "0.4.13";
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0dpv1am8qfz81jff99dhqpp6x73yfbpykv9yr2c29fdig77zvzbp";
+    sha256 = "1452vc93zpgv2kvl9vyzkbrb7dn6rabfn1hvmlnhnqrq3f3k5zrs";
   };
 
   propagatedBuildInputs = [ python-dateutil docutils pyparsing ];
 
-  meta = {
+  meta = with lib; {
     description = "Library for retrieving information about catkin packages";
-    homepage = http://wiki.ros.org/catkin_pkg;
+    homepage = "http://wiki.ros.org/catkin_pkg";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ lopsided98 ];
   };
 }
