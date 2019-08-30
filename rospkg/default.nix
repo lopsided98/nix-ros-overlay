8438dfc0096349a9704a4ea492a278dca9376e94
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-08-30 00:41:24 -0400

    pythonPackages.rospkg: 1.1.9 -> 1.1.10
    Now with upstream NixOS support.

--- rospkg/default.nix
+++ rospkg/default.nix
@@ -1,22 +1,22 @@
-{ buildPythonPackage, fetchPypi, catkin-pkg, pyyaml }:
+{ lib, buildPythonPackage, fetchPypi, catkin-pkg, pyyaml }:
 
 buildPythonPackage rec {
   pname = "rospkg";
-  version = "1.1.9";
-  
+  version = "1.1.10";
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0kkmvkc7m3j14xmq1523h8d1hvi4cv06myhxw3dxsdb4yf9qa0q0";
+    sha256 = "1picraw0ngj9ng0k7sdq7anfglk60i5w2rkag6zv294579aqp7zk";
   };
 
   propagatedBuildInputs = [ catkin-pkg pyyaml ];
 
-  patches = [ ./0001-os_detect-add-support-for-NixOS.patch ];
-
   setupHook = ./setup-hook.sh;
 
-  meta = {
+  meta = with lib; {
     description = "Library for retrieving information about ROS packages and stacks";
     homepage = http://wiki.ros.org/rospkg;
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ lopsided98 ];
   };
 }
