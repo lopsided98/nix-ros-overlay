338737a5fe3e86601d4232f60013cf99cd0af332
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-08-30 00:38:40 -0400

    pythonPackages.rosinstall-generator: 0.1.16 -> 0.1.17

--- rosinstall-generator/default.nix
+++ rosinstall-generator/default.nix
@@ -2,18 +2,19 @@
 
 buildPythonPackage rec {
   pname = "rosinstall_generator";
-  version = "0.1.16";
+  version = "0.1.17";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "15ckrcr16py0syxw5wg0yn0v6kdlr26ix57sm9qgy0zpz0ayw3i8";
+    sha256 = "1h08qwbafba4pnq7c9m7n4nl0b4zvz6jynaab3fpa816x5h2qsif";
   };
 
   propagatedBuildInputs = [ catkin-pkg rosdistro rospkg pyyaml ];
 
   meta = with lib; {
     description = "A tool to generator rosinstall files";
-    homepage = http://wiki.ros.org/rosinstall_generator;
+    homepage = "http://wiki.ros.org/rosinstall_generator";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ lopsided98 ];
   };
 }
