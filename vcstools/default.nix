bf670f244aa549579566a12e9fe05b1e35b46b77
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-08-30 00:44:11 -0400

    pythonPackages.vcstools: 0.1.40 -> 0.1.42

--- vcstools/default.nix
+++ vcstools/default.nix
@@ -1,12 +1,12 @@
-{ buildPythonPackage, fetchPypi, pyyaml, python-dateutil }:
+{ lib, buildPythonPackage, fetchPypi, pyyaml, python-dateutil }:
 
 buildPythonPackage rec {
   pname = "vcstools";
-  version = "0.1.40";
+  version = "0.1.42";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1mfasip71ky1g968n1zlramgn3fjxk4c922d0x9cs0nwm2snln4m";
+    sha256 = "0ykp0847fk16fffa2l29gp5hk9d11pqmpq2nmwwsgp8gignxhj4y";
   };
 
   propagatedBuildInputs = [ pyyaml python-dateutil ];
@@ -14,9 +14,11 @@ buildPythonPackage rec {
   # Tries to download files and lots of other issues
   doCheck = false;
 
-  meta = {
+  meta = with lib; {
     description = "Python library for interacting with various VCS systems";
-    homepage = http://wiki.ros.org/vcstools;
+    homepage = "http://wiki.ros.org/vcstools";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ lopsided98 ];
   };
 }
 
