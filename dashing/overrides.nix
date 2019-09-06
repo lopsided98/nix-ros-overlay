4950f6b5eeeaba32c1da8626acbcb91e155d9a4e
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-09-06 11:41:12 -0400

    Fix rosidl-generator-py on crystal.

--- dashing/overrides.nix
+++ dashing/overrides.nix
@@ -2,4 +2,15 @@
 self:
 # Distro package set
 rosSelf: rosSuper: {
+  rosidl-generator-py = rosSuper.rosidl-generator-py.overrideAttrs ({
+    patches ? [], ...
+  }: {
+     patches = patches ++ [
+      (self.fetchpatch {
+        url = "https://github.com/ros2/rosidl_python/commit/a7ce53c8922963439ba526f7f6c92dc0cc955c5f.patch";
+        sha256 = "0yal5mvhwxwkwzv5rlssfc4czck8zmcm36kwssypfwbkggc2700f";
+        stripLen = 1;
+      })
+    ];
+  });
 }
