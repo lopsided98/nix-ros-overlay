8597d662506f19c8c5e83ad8b61837eca19bd518
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-09-06 14:06:09 -0400

    Fix gazebo-plugins for ROS 2

--- kinetic/overrides.nix
+++ kinetic/overrides.nix
@@ -29,6 +29,18 @@ rosSelf: rosSuper: {
     ];
   });
 
+  gazebo-plugins = rosSuper.gazebo-plugins.overrideAttrs ({
+    patches ? [], ...
+  }:{
+    patches = patches ++ [
+      (self.fetchpatch {
+        url = "https://github.com/ros-simulation/gazebo_ros_pkgs/commit/b756d784d4bdc45bf33b28ac6c0a01c15563e36c.patch";
+        sha256 = "08lpkcda9yr3vjc2wxy5hzm99g7876d6402snac3mx5j7fa3w5i2";
+        stripLen = 1;
+      })
+    ];
+  });
+
   gazeboSimulator = self.gazeboSimulator // {
     gazebo = self.gazeboSimulator.gazebo7;
   };
@@ -106,7 +118,7 @@ rosSelf: rosSuper: {
       })
     ];
   });
-  
+
   realsense-camera = rosSuper.realsense-camera.overrideAttrs ({
     buildInputs ? [], ...
   }: {
@@ -123,7 +135,7 @@ rosSelf: rosSuper: {
       })
     ];
   });
-  
+
   tf2 = rosSuper.tf2.overrideAttrs ({
     patches ? [], ...
   }: {
--- melodic/overrides.nix
+++ melodic/overrides.nix
@@ -2,4 +2,15 @@
 self:
 # Distro package set
 rosSelf: rosSuper: {
+  gazebo-plugins = rosSuper.gazebo-plugins.overrideAttrs ({
+    patches ? [], ...
+  }:{
+    patches = patches ++ [
+      (self.fetchpatch {
+        url = "https://github.com/ros-simulation/gazebo_ros_pkgs/commit/b756d784d4bdc45bf33b28ac6c0a01c15563e36c.patch";
+        sha256 = "08lpkcda9yr3vjc2wxy5hzm99g7876d6402snac3mx5j7fa3w5i2";
+        stripLen = 1;
+      })
+    ];
+  });
 }
