9ac472b696eca2cfc4175e28b39a4397f53e3b6b
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-09-06 23:35:08 -0400

    dashing: fix nav2-util

--- dashing/overrides.nix
+++ dashing/overrides.nix
@@ -2,6 +2,18 @@
 self:
 # Distro package set
 rosSelf: rosSuper: {
+  nav2-util = rosSuper.nav2-util.overrideAttrs ({
+    patches ? [], ...
+  }: {
+     patches = patches ++ [
+      (self.fetchpatch {
+        url = "https://github.com/ros-planning/navigation2/commit/b87942a3502af9e4b6ea7d6cf78a510f55539cc1.patch";
+        sha256 = "1xhz0vkam7aka1xz6sin9ggdnqabxk1rcx3zmbrvjpvyp7aq37qj";
+        stripLen = 2;
+      })
+    ];
+  });
+
   rosidl-generator-py = rosSuper.rosidl-generator-py.overrideAttrs ({
     patches ? [], ...
   }: {
