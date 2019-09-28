bdfc6dd4574497d40c105d610e9fb3a25592242c
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-09-28 15:46:34 -0400

    build-ros-package: adapt to changes to buildPythonPackage

--- build-ros-package/default.nix
+++ build-ros-package/default.nix
@@ -10,9 +10,16 @@ else stdenv.mkDerivation) (args // {
   # Too difficult to fix all the problems with the tests in each package
   doCheck = false;
 
+  # ROS is sloppy with specifying build/runtime dependencies and
+  # buildPythonPackage turns on strictDeps by default
+  # FIXME: figure out a way to avoid this to eventually allow cross-compiling
+  strictDeps = false;
+
   passthru = passthru // {
     rosPackage = true;
   };
-} // lib.optionalAttrs (buildType != "ament_python") {
+} // (if buildType == "ament_python" then {
+  dontUseCmakeConfigure = true;
+} else {
   nativeBuildInputs = [ pythonPackages.wrapPython ] ++ nativeBuildInputs;
-})
+}))
