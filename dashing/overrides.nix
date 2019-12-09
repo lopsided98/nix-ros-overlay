97557f6dfa71ff629f373b87a74cade11497961c
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-12-09 15:58:07 -0500

    rosidl-generator-py: remove patch

--- dashing/overrides.nix
+++ dashing/overrides.nix
@@ -42,16 +42,4 @@ rosSelf: rosSuper: with rosSelf.lib; {
       })
     ];
   });
-
-  rosidl-generator-py = rosSuper.rosidl-generator-py.overrideAttrs ({
-    patches ? [], ...
-  }: {
-     patches = patches ++ [
-      (self.fetchpatch {
-        url = "https://github.com/ros2/rosidl_python/commit/a7ce53c8922963439ba526f7f6c92dc0cc955c5f.patch";
-        sha256 = "0yal5mvhwxwkwzv5rlssfc4czck8zmcm36kwssypfwbkggc2700f";
-        stripLen = 1;
-      })
-    ];
-  });
 }
