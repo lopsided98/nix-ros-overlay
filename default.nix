cc9a1d3e00ba1caf712cda90eb30fac7b74fc8df
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-01-13 12:07:13 -0500

    Automatically use my nixpkgs fork by default.

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
-{ nixpkgs ? <nixpkgs>, overlays ? [], ... }@args: import nixpkgs {
+{ nixpkgs ? builtins.fetchTarball "https://github.com/lopsided98/nixpkgs/archive/nix-ros.tar.gz"
+, overlays ? [], ... }@args: import nixpkgs {
   overlays = [ (import ./overlay.nix) ] ++ overlays;
 } // args
