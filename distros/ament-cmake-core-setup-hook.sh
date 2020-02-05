b601aa155530002d554e0f4a22930099d47648cd
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-02-05 01:13:29 -0500

    ament-cmake-core: use local_setup.sh for environment configuration

--- distros/ament-cmake-core-setup-hook.sh
+++ distros/ament-cmake-core-setup-hook.sh
@@ -7,10 +7,15 @@ declare -gA _amentPackagesSeen
 _findAmentPackages() {
   local pkg="$1"
   # Deduplicate the packages
-  if [ -z "${_amentPackagesSeen["$pkg"]:-}" ] && isAmentPackage "$pkg"; then
-    addToSearchPath AMENT_PREFIX_PATH "$pkg"
+  if [ -z "${_amentPackagesSeen["$pkg"]:-}" ] \
+    && isAmentPackage "$pkg" \
+    && [ -n "$(shopt -s nullglob; echo $pkg/share/*/local_setup.sh)" ]
+  then
+    # ROS scripts use unbound variables
+    set +u
+    source "$pkg"/share/*/local_setup.sh
+    set -u
   fi
   _amentPackagesSeen["$pkg"]=1
 }
 addEnvHooks "$hostOffset" _findAmentPackages
-
