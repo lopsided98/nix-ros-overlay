91c0bc970cd6354b0f5b818a4e94332a856237a3
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-03-07 14:00:43 -0500

    regenerate all distros, Sat Mar  7 14:00:43 2020

---
+++ distros/melodic/rosbag/default.nix
@@ -0,0 +1,27 @@
+
+# Copyright 2020 Open Source Robotics Foundation
+# Distributed under the terms of the BSD license
+
+{ lib, buildRosPackage, fetchurl, boost, catkin, cpp-common, genmsg, genpy, pythonPackages, rosbag-storage, rosconsole, roscpp, roscpp-serialization, roslib, rospy, std-srvs, topic-tools, xmlrpcpp }:
+buildRosPackage {
+  pname = "ros-melodic-rosbag";
+  version = "1.14.4-r1";
+
+  src = fetchurl {
+    url = "https://github.com/ros-gbp/ros_comm-release/archive/release/melodic/rosbag/1.14.4-1.tar.gz";
+    name = "1.14.4-1.tar.gz";
+    sha256 = "bdb1c2e5434908ae0ddd4b7ccac15d692d567ca089b024f3e817fb50d1a5d788";
+  };
+
+  buildType = "catkin";
+  buildInputs = [ cpp-common pythonPackages.pillow roscpp-serialization ];
+  propagatedBuildInputs = [ boost genmsg genpy pythonPackages.pycryptodome pythonPackages.python-gnupg pythonPackages.rospkg rosbag-storage rosconsole roscpp roslib rospy std-srvs topic-tools xmlrpcpp ];
+  nativeBuildInputs = [ catkin ];
+
+  meta = {
+    description = ''This is a set of tools for recording from and playing back to ROS
+    topics.  It is intended to be high performance and avoids
+    deserialization and reserialization of the messages.'';
+    license = with lib.licenses; [ bsdOriginal ];
+  };
+}
