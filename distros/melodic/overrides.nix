1f092abcbb0b802debb3d63c4d73dde4081a8cc8
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-05-23 15:26:18 -0400

    amcl: remove upstreamed patch

--- distros/melodic/overrides.nix
+++ distros/melodic/overrides.nix
@@ -2,18 +2,6 @@
 self:
 # Distro package set
 rosSelf: rosSuper: with rosSelf.lib; {
-  amcl = rosSuper.amcl.overrideAttrs ({
-    patches ? [], ...
-  }: {
-    patches = patches ++ [
-      (self.fetchpatch {
-        url = "https://github.com/ros-planning/navigation/commit/f6873e1f335b80cf7f0a5a6c327eabfcac88e6af.patch";
-        sha256 = "09477bglbibhfh12a954gbvdshn4c1j21s8c95m8v62qwm21jqld";
-        stripLen = 1;
-      })
-    ];
-  });
-
   gazeboSimulator = self.gazeboSimulator // {
     gazebo = self.gazeboSimulator.gazebo9;
   };
