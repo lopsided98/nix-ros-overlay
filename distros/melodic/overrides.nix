9cb66d5471c0e8a03fa7ea133afc3a0f54749f67
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-06-05 12:20:04 -0400

    melodic: pcl-ros: fix build with PCL >=1.11

--- distros/melodic/overrides.nix
+++ distros/melodic/overrides.nix
@@ -5,4 +5,17 @@ rosSelf: rosSuper: with rosSelf.lib; {
   gazeboSimulator = self.gazeboSimulator // {
     gazebo = self.gazeboSimulator.gazebo9;
   };
+  
+  pcl-ros = rosSuper.pcl-ros.overrideAttrs ({
+    patches ? [], ...
+  }: {
+    patches = patches ++ [
+      # Fix build with PCL >=1.11
+      (self.fetchpatch {
+        url = "https://github.com/ros-perception/perception_pcl/commit/e812d3cf1b67cc73841b41e690d53c74e5077a05.patch";
+        sha256 = "0savdifw90ql34zh6rff53hx3cy6rlmrwqzw80fsihkirls84b3w";
+        stripLen = 1;
+      })
+    ];
+  });
 }
