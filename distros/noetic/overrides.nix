4e7ff737733d0a696f3ebefd2bd11682b4120d8a
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-06-13 16:37:27 -0400

    noetic: fix pcl-ros

--- distros/noetic/overrides.nix
+++ distros/noetic/overrides.nix
@@ -10,4 +10,17 @@ rosSelf: rosSuper: with rosSelf.lib; {
     url = "https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22/libphidget22-1.6.20200417.tar.gz";
     sha256 = "17xfc07bgllqr808jm779z98gwz01iik9skw2hcwpgg4j7kdxn5d";
   };
+
+  pcl-ros = rosSuper.pcl-ros.overrideAttrs ({
+    patches ? [], ...
+  }: {
+    patches = patches ++ [
+      # Fix build with PCL >=1.11
+      (self.fetchpatch {
+        url = "https://github.com/ros-perception/perception_pcl/commit/e812d3cf1b67cc73841b41e690d53c74e5077a05.patch";
+        sha256 = "0savdifw90ql34zh6rff53hx3cy6rlmrwqzw80fsihkirls84b3w";
+        stripLen = 1;
+      })
+    ];
+  });
 }
