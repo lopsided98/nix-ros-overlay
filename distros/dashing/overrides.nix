bf5f5c3029badd01763efb20fd087b4259a79a23
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-08-02 20:12:47 -0400

    Fix cv-bridge patches.

--- distros/dashing/overrides.nix
+++ distros/dashing/overrides.nix
@@ -36,8 +36,8 @@ rosSelf: rosSuper: with rosSelf.lib; {
     propagatedBuildInputs ? [], ...
   }: {
     patches = patches ++ [ (self.fetchpatch {
-      url = "https://github.com/ros-perception/vision_opencv/commit/fbfb25303bd77db6a5943043cc4303d0c0b835c9.patch";
-      sha256 = "0pznlbqp99f6bzql3q1sjy9sqk2746wgp7qdqj790j5z0vb2v2r6";
+      url = "https://github.com/ros-perception/vision_opencv/commit/f41fc9b761b4e12f5164fb93696c6222ba3232a6.patch";
+      sha256 = "10mw2k4a3pypiq194grk5minv61zlqbmcd4s3qvqbrlg2pnl5q9b";
       stripLen = 1;
     }) ];
     propagatedBuildInputs = propagatedBuildInputs ++ [ rosSelf.pythonPackages.opencv3 ];
