59a59422413075b766c82fa1b32aba1bb01b2814
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-08-24 18:28:26 -0400

    buildRosPackage: allow overriding all attributes

--- distros/build-ros-package/default.nix
+++ distros/build-ros-package/default.nix
@@ -1,23 +1,23 @@
 { stdenv, lib, pythonPackages }:
 { buildType ? "catkin"
-, nativeBuildInputs ? []
+  # Too difficult to fix all the problems with the tests in each package
+, doCheck ? false
+  # ROS is sloppy with specifying build/runtime dependencies and
+  # buildPythonPackage turns on strictDeps by default
+  # FIXME: figure out a way to avoid this to eventually allow cross-compiling
+, strictDeps ? false
+, CXXFLAGS ? ""
 , passthru ? {}
 , ...
 }@args:
 
 (if buildType == "ament_python" then pythonPackages.buildPythonPackage
 else stdenv.mkDerivation) (args // {
-  # Too difficult to fix all the problems with the tests in each package
-  doCheck = false;
-
-  # ROS is sloppy with specifying build/runtime dependencies and
-  # buildPythonPackage turns on strictDeps by default
-  # FIXME: figure out a way to avoid this to eventually allow cross-compiling
-  strictDeps = false;
+  inherit doCheck strictDeps;
 
   # Disable warnings that cause "Log limit exceeded" errors on Hydra in lots of
   # packages that use Eigen
-  CXXFLAGS = "-Wno-deprecated-declarations -Wno-deprecated-copy";
+  CXXFLAGS = CXXFLAGS + "-Wno-deprecated-declarations -Wno-deprecated-copy";
 
   passthru = passthru // {
     rosPackage = true;
