28c1c1a7b1922d750cc6e348642f9cf77d7858c1
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-09-21 20:53:46 -0400

    Add basic Nix flake.

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "ROS overlay for the Nix package manager";
+
+  inputs = {
+    nixpkgs.url = "github:lopsided98/nixpkgs/nix-ros";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    eachSystem allSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in {
+      packages = pkgs.rosPackages;
+    }) // {
+      overlay = import ./overlay.nix;
+    };
+}
