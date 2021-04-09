04acc33410d6a5544a4d9f621d43288dbd4a4e52
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-04-09 13:56:04 -0400

    rosinstall-generator: 0.1.18 -> 0.1.22, fix GitLab downloads

--- pkgs/rosinstall-generator/default.nix
+++ pkgs/rosinstall-generator/default.nix
@@ -1,15 +1,26 @@
-{ lib, buildPythonPackage, fetchPypi, catkin-pkg, rosdistro, rospkg, pyyaml }:
+{ lib, buildPythonPackage, fetchPypi, fetchpatch, catkin-pkg, rosdistro, rospkg
+, pyyaml, pytestCheckHook }:
 
 buildPythonPackage rec {
   pname = "rosinstall_generator";
-  version = "0.1.18";
+  version = "0.1.22";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "18n1yyadr88hm0wlqzpr4gbyjm58kbl2i4d61vc8gij6a3fxxknz";
+    sha256 = "1idq8xacz7s59mb7aakma3g2w1n89i3dkcdjgzvs221zrncjbli2";
   };
 
+  patches = [
+    # Fix GitLab tarball downloads
+    (fetchpatch {
+      url = "https://github.com/lopsided98/rosinstall_generator/commit/16291631160ba9801ea5b551d6840d692acf1e44.patch";
+      sha256 = "0ynbq2n5qd49a72ylcp8cim02b2srmr1956ifp6cww1s1jrvbhgc";
+    })
+  ];
+
   propagatedBuildInputs = [ catkin-pkg rosdistro rospkg pyyaml ];
+
+  checkInputs = [ pytestCheckHook ];
 
   meta = with lib; {
     description = "A tool to generator rosinstall files";
