a4122ce1bcc39d012b4eb85a6e91e85e8a7a99b6
Superflore <superflore@github.com>
2021-05-29 02:26:02 +0000

    regenerate all distros, Sat May 29 02:26:02 2021

---
+++ distros/foxy/moveit-core/default.nix
@@ -0,0 +1,26 @@
+
+# Copyright 2021 Open Source Robotics Foundation
+# Distributed under the terms of the BSD license
+
+{ lib, buildRosPackage, fetchurl, ament-cmake, ament-cmake-gtest, ament-index-cpp, ament-lint-auto, ament-lint-common, angles, assimp, boost, bullet, common-interfaces, eigen, eigen-stl-containers, eigen3-cmake-module, fcl, geometric-shapes, geometry-msgs, kdl-parser, moveit-common, moveit-msgs, moveit-resources-panda-moveit-config, moveit-resources-pr2-description, octomap, octomap-msgs, orocos-kdl, pkg-config, random-numbers, rclcpp, sensor-msgs, shape-msgs, srdfdom, std-msgs, tf2, tf2-eigen, tf2-geometry-msgs, tf2-kdl, trajectory-msgs, urdf, urdfdom, urdfdom-headers, visualization-msgs }:
+buildRosPackage {
+  pname = "ros-foxy-moveit-core";
+  version = "2.1.3-r1";
+
+  src = fetchurl {
+    url = "https://github.com/moveit/moveit2-release/archive/release/foxy/moveit_core/2.1.3-1.tar.gz";
+    name = "2.1.3-1.tar.gz";
+    sha256 = "a0c03e5809e00caa733d0bd5d447a48d5bf70a07fb4d7a96148d4c9bf7107cf8";
+  };
+
+  buildType = "ament_cmake";
+  buildInputs = [ moveit-common ];
+  checkInputs = [ ament-cmake-gtest ament-index-cpp ament-lint-auto ament-lint-common angles moveit-resources-panda-moveit-config moveit-resources-pr2-description orocos-kdl tf2-kdl ];
+  propagatedBuildInputs = [ angles assimp boost bullet common-interfaces eigen eigen-stl-containers eigen3-cmake-module fcl geometric-shapes geometry-msgs kdl-parser moveit-msgs octomap octomap-msgs random-numbers rclcpp sensor-msgs shape-msgs srdfdom std-msgs tf2 tf2-eigen tf2-geometry-msgs trajectory-msgs urdf urdfdom urdfdom-headers visualization-msgs ];
+  nativeBuildInputs = [ ament-cmake eigen3-cmake-module pkg-config ];
+
+  meta = {
+    description = ''Core libraries used by MoveIt'';
+    license = with lib.licenses; [ bsdOriginal ];
+  };
+}
