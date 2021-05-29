c01dce8c0096dc1843819e930a58f1b9c6528f48
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-05-29 18:17:19 -0400

    noetic: pybind11-catkin: patch vendor url

--- distros/noetic/overrides.nix
+++ distros/noetic/overrides.nix
@@ -11,6 +11,11 @@ rosSelf: rosSuper: with rosSelf.lib; {
     sha256 = "17xfc07bgllqr808jm779z98gwz01iik9skw2hcwpgg4j7kdxn5d";
   };
 
+  pybind11-catkin = patchVendorUrl rosSuper.pybind11-catkin {
+    url = "https://github.com/pybind/pybind11/archive/v2.5.0.zip";
+    sha256 = "080mss10aadal8sk3bib2dr2ygx6hmxn2g92qsq42v3zhchz2n8q";
+  };
+
   rosfmt = patchVendorUrl rosSuper.rosfmt {
     url = "https://github.com/fmtlib/fmt/releases/download/7.1.2/fmt-7.1.2.zip";
     sha256 = "19qfd19mvzg4awqbh5x10m8riyyy0dbpadpidp3mrs81gjmnhsad";
