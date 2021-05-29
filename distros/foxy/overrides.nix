081ba4e3525257a4092f9f805494310bd5152820
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-05-28 20:54:58 -0400

    foxy: remove unecessary vendor patches
    I don't think these are required, as the packages build without them.

--- distros/foxy/overrides.nix
+++ distros/foxy/overrides.nix
@@ -2,11 +2,6 @@
 self:
 # Distro package set
 rosSelf: rosSuper: with rosSelf.lib; {
-  console-bridge-vendor = patchVendorUrl rosSuper.console-bridge-vendor {
-    url = "https://github.com/ros/console_bridge/archive/0.5.1.tar.gz";
-    sha256 = "0c7ivb99jjfimxb28chlc3pjnmzdz88f5abkh83x846m5k461bf4";
-  };
-
   gazebo = self.gazebo_11;
 
   libphidget22 = patchVendorUrl rosSuper.libphidget22 {
@@ -17,10 +12,5 @@ rosSelf: rosSuper: with rosSelf.lib; {
   rviz-ogre-vendor = patchVendorUrl rosSuper.rviz-ogre-vendor {
     url = "https://github.com/OGRECave/ogre/archive/v1.12.1.zip";
     sha256 = "1iv6k0dwdzg5nnzw2mcgcl663q4f7p2kj7nhs8afnsikrzxxgsi4";
-  };
-
-  spdlog-vendor = patchVendorUrl rosSuper.spdlog-vendor {
-    url = "https://github.com/gabime/spdlog/archive/v1.5.0.tar.gz";
-    sha256 = "12pfi87vh5pjv17iscnm6xkpykhdkg0s0l6mzs12pb7syyz0p3mk";
   };
 }
