97f32cb6313d5272d950909780044cce62113e93
Mike Purvis <mpurvis@clearpath.ai>
2021-09-23 20:35:04 +0000

    Add output, notification, parallel plugins for colcon.

---
+++ pkgs/colcon/output.nix
@@ -0,0 +1,24 @@
+{ lib, buildPythonPackage, fetchPypi, isPy27, colcon-core }:
+
+buildPythonPackage rec {
+  pname = "colcon-output";
+  version = "0.2.12";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-ohHp8fcO2xVnwHR1Mq0iLkd5nO8ly4Y+SkOvRmB5izA=";
+  };
+
+  propagatedBuildInputs = [ colcon-core ];
+
+  doCheck = false;
+
+  disabled = isPy27;
+
+  meta = with lib; {
+    description = "An extension for colcon-core to customize the output in various ways.";
+    homepage = "https://colcon.readthedocs.io";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ lopsided98 ];
+  };
+}
