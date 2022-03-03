a405f6270f47cdcd7dd98cecf41b228ff02fe8ea
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-03-03 00:58:48 -0500

    Revert "gazebo: use Ogre 1.12"
    This reverts commit 6178a8c42f31f1a70d9fec3e68c4bfb8e1e299c0.
    
    Apparently, 1.10 works correctly, but I haven't tested.

--- pkgs/gazebo/9.nix
+++ pkgs/gazebo/9.nix
@@ -1,10 +1,9 @@
-{ callPackage, sdformat_6, ogre1_10, ignition, ... } @ args:
+{ callPackage, sdformat_6, ignition, ... } @ args:
 
 callPackage ./. (args // {
   version = "9.18.0";
   srcSha256 = "0j9458bzm7a3pl4nsialypw2s3p1fk3sjpji8kw8aqkrdrpdw03d";
   sdformat = sdformat_6;
-  ogre = ogre1_10;
   ignition-common = ignition.common1;
   ignition-transport = ignition.transport4;
   ignition-msgs = ignition.msgs1;
