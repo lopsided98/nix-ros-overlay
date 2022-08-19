624fc3c9767269d360d1b4895423ad6ff625be64
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-08-18 20:08:29 -0400

    gazebo_9: add patch to fix build
    With compatibility with QWT6.

--- pkgs/gazebo/default.nix
+++ pkgs/gazebo/default.nix
@@ -21,16 +21,21 @@ mkDerivation rec {
   };
 
   patches =
-    # Fix compatibility with OpenAL v1.20.0
-    lib.optional (lib.versionOlder version "11.0.0") (fetchpatch {
-      url = "https://github.com/osrf/gazebo/commit/b4429a242c43e01c367bc02afa2c4a0e6b59dddd.patch";
-      sha256 = "045dp4pr0gn5ns84w785r4x2g0dfrkrb9wy5pk0yqwq6azsj6v23";
-    }) ++
     # Fix compatibility with qwt headers not in subdirectory
     # https://github.com/osrf/gazebo/pull/2887
     lib.optional (lib.versionOlder version "11.3.0") (fetchpatch {
       url = "https://github.com/osrf/gazebo/commit/25d3381c083a9eeafcee34ef648339a83e192676.patch";
       sha256 = "1qixrz2jiqdc37mgcsnv562m7mzr6w0rd67fmkr5710n6dnky4y7";
+    }) ++
+    # Fix compatibility with QWT 6.2
+    lib.optional (lib.versionOlder version "9.20.0") (fetchpatch {
+      url = "https://github.com/osrf/gazebo/commit/2fc9018c17cf44e28548d571b70f7543c1e3a5f3.patch";
+      hash = "sha256-9BB9MQbDSvbA13TPfSZR5XmrFo2JIopaDkscqGZ/HNU=";
+    }) ++
+    # Fix compatibility with OpenAL v1.20.0
+    lib.optional (lib.versionOlder version "11.0.0") (fetchpatch {
+      url = "https://github.com/osrf/gazebo/commit/b4429a242c43e01c367bc02afa2c4a0e6b59dddd.patch";
+      hash = "sha256-Q2wj9VcGc+zBvMXztPLMroEnOskFHU6QtsU+kC+5rRA=";
     });
 
   enableParallelBuilding = true;
