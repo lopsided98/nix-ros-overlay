a680886058a31641a2a3bb5c669d1eb971b3b161
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-10-21 14:04:26 -0400

    python3Packages.rospkg: 1.1.10 -> 1.4.0

--- pkgs/rospkg/default.nix
+++ pkgs/rospkg/default.nix
@@ -1,21 +1,21 @@
-{ lib, buildPythonPackage, fetchPypi, catkin-pkg, pyyaml }:
+{ lib, buildPythonPackage, fetchPypi, catkin-pkg, pyyaml, distro }:
 
 buildPythonPackage rec {
   pname = "rospkg";
-  version = "1.1.10";
+  version = "1.4.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1picraw0ngj9ng0k7sdq7anfglk60i5w2rkag6zv294579aqp7zk";
+    hash = "sha256-7yJFYApTRayMna1S+mpPvGvM/KQREer50bOZqQVcANQ=";
   };
 
-  propagatedBuildInputs = [ catkin-pkg pyyaml ];
+  propagatedBuildInputs = [ catkin-pkg pyyaml distro ];
 
   setupHook = ./setup-hook.sh;
 
   meta = with lib; {
     description = "Library for retrieving information about ROS packages and stacks";
-    homepage = "http://wiki.ros.org/rospkg";
+    homepage = "https://wiki.ros.org/rospkg";
     license = licenses.bsd3;
     maintainers = with maintainers; [ lopsided98 ];
   };
