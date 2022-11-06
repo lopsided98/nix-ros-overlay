41d7e9a2c471df01f6e6e4f1268c178ec00fec03
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-11-06 16:25:51 -0500

    Fix passing through arguments to nixpkgs.

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
 { nixpkgs ? builtins.fetchTarball "https://github.com/lopsided98/nixpkgs/archive/nix-ros.tar.gz"
-, overlays ? [], ... }@args: import nixpkgs {
+, overlays ? [], ... }@args: import nixpkgs ({
   overlays = [ (import ./overlay.nix) ] ++ overlays;
-} // args
+} // args)
