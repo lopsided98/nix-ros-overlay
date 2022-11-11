af236198c3a2d2183ebb61dc438b07e0bd335339
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-11-11 16:14:22 -0500

    sdformat_6: add patches to fix build

--- pkgs/sdformat/default.nix
+++ pkgs/sdformat/default.nix
@@ -15,12 +15,22 @@ stdenv.mkDerivation rec {
     hash = srcHash;
   };
 
-  # Fix asssumptions that CMAKE_INSTALL_*DIR variables are relative
-  # https://github.com/gazebosim/sdformat/pull/1190
-  patches = lib.optional (lib.versionAtLeast version "9") (fetchpatch {
-    url = "https://github.com/gazebosim/sdformat/commit/e8286cd012623173f40536c7fbea780bb65e7f0b.patch";
-    hash = "sha256-WHpbN2n3JDV98SzWpPRtFE4xRB/jdlwMZagTsIZ1ixk=";
-  });
+  patches =
+    # Fix asssumptions that CMAKE_INSTALL_*DIR variables are relative
+    # https://github.com/gazebosim/sdformat/pull/1190
+    lib.singleton (if (lib.versionAtLeast version "9") then {
+      url = "https://github.com/gazebosim/sdformat/commit/e8286cd012623173f40536c7fbea780bb65e7f0b.patch";
+      hash = "sha256-WHpbN2n3JDV98SzWpPRtFE4xRB/jdlwMZagTsIZ1ixk=";
+    } else fetchpatch {
+      url = "https://github.com/gazebosim/sdformat/commit/b37a28eefb4c06e1add5fa53d7a08cd5d2402f9b.patch";
+      hash = "sha256-IZ5RIl3GlAY4LzcmOOpiWZkyjVnsislo5brwu+t5pZo=";
+    }) ++
+    # Fix finding urdfdom headers
+    # Backport of: https://github.com/gazebosim/sdformat/commit/eddf3ef00f07549e7270f619de1ae2849d49daaa
+    lib.optional (lib.versionOlder version "8.9.0") (fetchpatch {
+      url = "https://github.com/gazebosim/sdformat/commit/6745b2e9a2c7b90ef07232292e3679b79190b1d7.patch";
+      hash = "sha256-ntsmfQkNdhGlKMifoG0khKuGmL4tmd/UF75Pp3d0yE4=";
+    });
 
   enableParallelBuilding = true;
 
