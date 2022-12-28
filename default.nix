983d3e8da95046a03fb1ae2739a3bb25e0d435a5
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-12-28 11:54:48 -0500

    Fix applying extra overlays in default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
 { nixpkgs ? builtins.fetchTarball "https://github.com/lopsided98/nixpkgs/archive/nix-ros.tar.gz"
-, overlays ? [], ... }@args: import nixpkgs ({
+, overlays ? []
+, ... }@args:
+
+import nixpkgs (args // {
   overlays = [ (import ./overlay.nix) ] ++ overlays;
-} // args)
+})
