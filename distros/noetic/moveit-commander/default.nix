8c940407f75a01851d01cb38fefa6979f8b4ac94
Jeff Hykin <jeff.hykin@gmail.com>
2023-02-08 13:15:25 -0600

    -

--- distros/noetic/moveit-commander/default.nix
+++ distros/noetic/moveit-commander/default.nix
@@ -2,7 +2,7 @@
 # Copyright 2023 Open Source Robotics Foundation
 # Distributed under the terms of the BSD license
 
-{ lib, buildRosPackage, fetchurl, catkin, geometry-msgs, moveit-msgs, moveit-resources-fanuc-moveit-config, moveit-ros-planning-interface, python3, python3Packages, rospy, rostest, sensor-msgs, shape-msgs, tf }:
+{ lib, buildRosPackage, fetchurl, catkin, geometry-msgs, moveit-msgs, moveit-resources-fanuc-moveit-config, moveit-ros-planning-interface, python3, python3Packages, rospy, rostest, sensor-msgs, shape-msgs, tf, assimp }:
 buildRosPackage {
   pname = "ros-noetic-moveit-commander";
   version = "1.1.11-r1";
@@ -20,7 +20,7 @@ buildRosPackage {
   buildType = "catkin";
   buildInputs = [ catkin python3Packages.catkin-pkg ];
   checkInputs = [ moveit-resources-fanuc-moveit-config rostest ];
-  propagatedBuildInputs = [ geometry-msgs moveit-msgs moveit-ros-planning-interface python3 rospy sensor-msgs shape-msgs tf ];
+  propagatedBuildInputs = [ geometry-msgs moveit-msgs moveit-ros-planning-interface python3 rospy sensor-msgs shape-msgs tf assimp ];
   nativeBuildInputs = [ catkin python3Packages.catkin-pkg ];
 
   meta = {
