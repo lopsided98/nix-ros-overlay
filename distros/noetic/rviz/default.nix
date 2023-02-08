e74242e4e304935d0df948ba396440cf3d0542e8
Jeff Hykin <jeff.hykin@gmail.com>
2023-02-08 14:43:53 -0600

    -

--- distros/noetic/rviz/default.nix
+++ distros/noetic/rviz/default.nix
@@ -7,28 +7,29 @@ let
     python = python3.withPackages(python3Packages: [
         python3Packages.sip_4
     ]);
-buildRosPackage {
-  pname = "ros-noetic-rviz";
-  version = "1.14.19-r1";
-
-  src = let
-      fetchFromGithub = (builtins.import (builtins.fetchTarball ({ url = "https://github.com/NixOS/nixpkgs/archive/aa0e8072a57e879073cee969a780e586dbe57997.tar.gz"; })) ({})).fetchFromGitHub;
-    in
-      fetchFromGithub {
-        owner = "ros-gbp";
-        repo = "rviz-release";
-        rev = "release/noetic/rviz/1.14.19-1";
-        sha256 = "sha256-Oi6jSUrbDPDkPW/nl1gKp5np7yk1VofHwzROt8B8mWg=";
-      };
-
-  buildType = "catkin";
-  buildInputs = [ catkin cmake-modules eigen message-generation urdfdom urdfdom-headers python ];
-  checkInputs = [ rostest rosunit ];
-  propagatedBuildInputs = [ assimp geometry-msgs image-transport interactive-markers laser-geometry libGL libGLU libyamlcpp map-msgs media-export message-filters message-runtime nav-msgs ogre1_9 pluginlib python-qt-binding qt5.qtbase resource-retriever rosconsole roscpp roslib rospy sensor-msgs std-msgs std-srvs tf2-geometry-msgs tf2-ros tinyxml-2 urdf visualization-msgs python ];
-  nativeBuildInputs = [ catkin ];
-
-  meta = {
-    description = ''3D visualization tool for ROS.'';
-    license = with lib.licenses; [ bsdOriginal "Creative-Commons" ];
-  };
-}
+in
+  buildRosPackage {
+    pname = "ros-noetic-rviz";
+    version = "1.14.19-r1";
+  
+    src = let
+        fetchFromGithub = (builtins.import (builtins.fetchTarball ({ url = "https://github.com/NixOS/nixpkgs/archive/aa0e8072a57e879073cee969a780e586dbe57997.tar.gz"; })) ({})).fetchFromGitHub;
+      in
+        fetchFromGithub {
+          owner = "ros-gbp";
+          repo = "rviz-release";
+          rev = "release/noetic/rviz/1.14.19-1";
+          sha256 = "sha256-Oi6jSUrbDPDkPW/nl1gKp5np7yk1VofHwzROt8B8mWg=";
+        };
+  
+    buildType = "catkin";
+    buildInputs = [ catkin cmake-modules eigen message-generation urdfdom urdfdom-headers python ];
+    checkInputs = [ rostest rosunit ];
+    propagatedBuildInputs = [ assimp geometry-msgs image-transport interactive-markers laser-geometry libGL libGLU libyamlcpp map-msgs media-export message-filters message-runtime nav-msgs ogre1_9 pluginlib python-qt-binding qt5.qtbase resource-retriever rosconsole roscpp roslib rospy sensor-msgs std-msgs std-srvs tf2-geometry-msgs tf2-ros tinyxml-2 urdf visualization-msgs python ];
+    nativeBuildInputs = [ catkin ];
+  
+    meta = {
+      description = ''3D visualization tool for ROS.'';
+      license = with lib.licenses; [ bsdOriginal "Creative-Commons" ];
+    };
+  }
