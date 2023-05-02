7b8a2198b57249a89dda5ac4958a96718db806eb
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-05-02 17:16:04 -0400

    rosinstall-generator: unstable-20211203 -> 0.1.23

--- pkgs/rosinstall-generator/default.nix
+++ pkgs/rosinstall-generator/default.nix
@@ -3,14 +3,13 @@
 
 buildPythonPackage rec {
   pname = "rosinstall_generator";
-  version = "unstable-20211203";
+  version = "0.1.23";
 
   src = fetchFromGitHub {
     owner = "ros-infrastructure";
     repo = pname;
-    # Contains several important fixes
-    rev = "9024a02e0aa0e09b3a533458e05a4e862eae7ddf";
-    sha256 = "sha256-yDKQQyZE75rYDS6hL638fOJxaqkNEQ/g5NG0eBEY/K0=";
+    rev = version;
+    hash = "sha256-4Kan5MGfr4dMsSNTBf4RXsC4ae8mzeU2tgBdAsOw2IY=";
   };
 
   propagatedBuildInputs = [ catkin-pkg rosdistro rospkg pyyaml ];
