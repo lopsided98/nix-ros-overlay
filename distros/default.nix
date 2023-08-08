5384a9b63e90f1f83f9d41fd430fb86112d5fe98
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-08-08 16:29:26 -0400

    Remove melodic.
    Melodic has been EOL since June 27, 2023 and is mostly broken anyway.

--- distros/default.nix
+++ distros/default.nix
@@ -5,12 +5,6 @@ self: super: {
     recurseForDerivations = true;
     lib = super.lib // import ../lib { inherit lib self; };
 
-    melodicPython3 = import ./distro-overlay.nix {
-      version = 1;
-      distro = "melodic";
-      python = self.python3;
-    } self super;
-
     noetic = import ./distro-overlay.nix {
       version = 1;
       distro = "noetic";
