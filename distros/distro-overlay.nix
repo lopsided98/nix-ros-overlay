0aa4c3092aa5a7e483cebfe6877441aed16f8901
Igor Brylyov <movefasta@dezcom.org>
2024-02-13 17:30:42 +0300

    fix librealsense2 compile error

--- distros/distro-overlay.nix
+++ distros/distro-overlay.nix
@@ -90,9 +90,13 @@ let
     });
 
     librealsense2 = rosSuper.librealsense2.overrideAttrs ({
-      buildInputs ? [], ...
+      buildInputs ? [], patches ? [], ...
     }: {
       buildInputs = buildInputs ++ [ self.glfw self.libGLU ];
+      patches = patches ++ [(self.fetchpatch {
+        url = "https://github.com/IntelRealSense/librealsense/commit/847b74d3dcade2842ba138f321474159315ab8c2.patch";
+        sha256 = "sha256-zaW8HG8rfsApI5S/3x+x9Fx8xhyTIPNn/fJVFtkmlEA=";
+      })];
     });
 
     libuvc-camera = rosSuper.libuvc-camera.overrideAttrs ({
