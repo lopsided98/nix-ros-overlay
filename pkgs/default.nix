9511104fcbd529d37df9828fdb9c971568f26988
Ben Wolsieffer <benwolsieffer@gmail.com>
2024-07-20 19:21:54 -0400

    pkgs: use pythonPackagesExtensions
    This should avoid problems composing Python overrides.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,77 +1,4 @@
-self: super: with self.lib; let
-
-  pythonOverridesFor = prevPython: prevPython // {
-    pkgs = prevPython.pkgs.overrideScope (pyFinal: pyPrev: {
-      bson = pyFinal.callPackage ./bson { };
-
-      catkin-pkg = pyFinal.callPackage ./catkin-pkg { };
-
-      catkin-tools = pyFinal.callPackage ./catkin-tools { };
-
-      colcon-argcomplete = pyFinal.callPackage ./colcon/argcomplete.nix { };
-
-      colcon-bash = pyFinal.callPackage ./colcon/bash.nix { };
-
-      colcon-cargo = pyFinal.callPackage ./colcon/cargo.nix { };
-
-      colcon-cmake = pyFinal.callPackage ./colcon/cmake.nix { };
-
-      colcon-core = pyFinal.callPackage ./colcon/core.nix { };
-
-      colcon-defaults = pyFinal.callPackage ./colcon/defaults.nix { };
-
-      colcon-devtools = pyFinal.callPackage ./colcon/devtools.nix { };
-
-      colcon-library-path = pyFinal.callPackage ./colcon/library-path.nix { };
-
-      colcon-metadata = pyFinal.callPackage ./colcon/metadata.nix { };
-
-      colcon-notification = pyFinal.callPackage ./colcon/notification.nix { };
-
-      colcon-output = pyFinal.callPackage ./colcon/output.nix { };
-
-      colcon-package-information = pyFinal.callPackage ./colcon/package-information.nix { };
-
-      colcon-package-selection = pyFinal.callPackage ./colcon/package-selection.nix { };
-
-      colcon-parallel-executor = pyFinal.callPackage ./colcon/parallel-executor.nix { };
-
-      colcon-pkg-config = pyFinal.callPackage ./colcon/pkg-config.nix { };
-
-      colcon-python-setup-py = pyFinal.callPackage ./colcon/python-setup-py.nix { };
-
-      colcon-recursive-crawl = pyFinal.callPackage ./colcon/recursive-crawl.nix { };
-
-      colcon-ros = pyFinal.callPackage ./colcon/ros.nix { };
-
-      colcon-ros-cargo = pyFinal.callPackage ./colcon/ros-cargo.nix { };
-
-      colcon-test-result = pyFinal.callPackage ./colcon/test-result.nix { };
-
-      colcon-zsh = pyFinal.callPackage ./colcon/zsh.nix { };
-
-      empy_3 = pyPrev.empy.overrideAttrs ({
-        pname, ...
-      }: rec {
-        version = "3.3.4";
-        src = pyFinal.fetchPypi {
-          inherit pname version;
-          hash = "sha256-c6xJeFtgFHnfTqGKfHm8EwSop8NMArlHLPEgauiPAbM=";
-        };
-      });
-
-      osrf-pycommon = pyFinal.callPackage ./osrf-pycommon {};
-
-      rosdep = pyFinal.callPackage ./rosdep { };
-
-      rosdistro = pyFinal.callPackage ./rosdistro { };
-
-      rosinstall-generator = pyFinal.callPackage ./rosinstall-generator { };
-
-      rospkg = pyFinal.callPackage ./rospkg { };
-    });
-  };
-in {
+self: super: with self.lib; {
   cargo-ament-build = self.callPackage ./cargo-ament-build { };
 
   colcon = with self.python3Packages; colcon-core.withExtensions [
@@ -144,12 +71,77 @@ in {
   opensplice_6_9 = self.callPackage ./opensplice { };
   opensplice = self.opensplice_6_9;
 
-  python27 = pythonOverridesFor super.python27;
-  python37 = pythonOverridesFor super.python37;
-  python38 = pythonOverridesFor super.python38;
-  python39 = pythonOverridesFor super.python39;
-  python310 = pythonOverridesFor super.python310;
-  python311 = pythonOverridesFor super.python311;
+  pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
+    (pyFinal: pyPrev: {
+      bson = pyFinal.callPackage ./bson { };
+
+      catkin-pkg = pyFinal.callPackage ./catkin-pkg { };
+
+      catkin-tools = pyFinal.callPackage ./catkin-tools { };
+
+      colcon-argcomplete = pyFinal.callPackage ./colcon/argcomplete.nix { };
+
+      colcon-bash = pyFinal.callPackage ./colcon/bash.nix { };
+
+      colcon-cargo = pyFinal.callPackage ./colcon/cargo.nix { };
+
+      colcon-cmake = pyFinal.callPackage ./colcon/cmake.nix { };
+
+      colcon-core = pyFinal.callPackage ./colcon/core.nix { };
+
+      colcon-defaults = pyFinal.callPackage ./colcon/defaults.nix { };
+
+      colcon-devtools = pyFinal.callPackage ./colcon/devtools.nix { };
+
+      colcon-library-path = pyFinal.callPackage ./colcon/library-path.nix { };
+
+      colcon-metadata = pyFinal.callPackage ./colcon/metadata.nix { };
+
+      colcon-notification = pyFinal.callPackage ./colcon/notification.nix { };
+
+      colcon-output = pyFinal.callPackage ./colcon/output.nix { };
+
+      colcon-package-information = pyFinal.callPackage ./colcon/package-information.nix { };
+
+      colcon-package-selection = pyFinal.callPackage ./colcon/package-selection.nix { };
+
+      colcon-parallel-executor = pyFinal.callPackage ./colcon/parallel-executor.nix { };
+
+      colcon-pkg-config = pyFinal.callPackage ./colcon/pkg-config.nix { };
+
+      colcon-python-setup-py = pyFinal.callPackage ./colcon/python-setup-py.nix { };
+
+      colcon-recursive-crawl = pyFinal.callPackage ./colcon/recursive-crawl.nix { };
+
+      colcon-ros = pyFinal.callPackage ./colcon/ros.nix { };
+
+      colcon-ros-cargo = pyFinal.callPackage ./colcon/ros-cargo.nix { };
+
+      colcon-test-result = pyFinal.callPackage ./colcon/test-result.nix { };
+
+      colcon-zsh = pyFinal.callPackage ./colcon/zsh.nix { };
+
+      empy_3 = pyPrev.empy.overrideAttrs ({
+        pname, ...
+      }: rec {
+        version = "3.3.4";
+        src = pyFinal.fetchPypi {
+          inherit pname version;
+          hash = "sha256-c6xJeFtgFHnfTqGKfHm8EwSop8NMArlHLPEgauiPAbM=";
+        };
+      });
+
+      osrf-pycommon = pyFinal.callPackage ./osrf-pycommon {};
+
+      rosdep = pyFinal.callPackage ./rosdep { };
+
+      rosdistro = pyFinal.callPackage ./rosdistro { };
+
+      rosinstall-generator = pyFinal.callPackage ./rosinstall-generator { };
+
+      rospkg = pyFinal.callPackage ./rospkg { };
+    })
+  ];
 
   sdformat_9 = self.callPackage ./sdformat/9.nix { };
   sdformat_13 = self.callPackage ./sdformat/13.nix { };
