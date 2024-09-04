dde852afc9f761d150950d44c8b03ee5a19dc73e
Michal Sojka <michal.sojka@cvut.cz>
2024-09-04 23:56:56 +0200

    Add script and Github workflow to automate colcon updates

---
+++ maintainers/scripts/update-colcon.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash --pure
+#! nix-shell -p jq nix nix-update cacert git
+
+# Usage: update-colcon.sh [nix-update options]
+
+set -euo pipefail
+
+nix eval --json --impure --expr '
+let
+  pkgs = (import ./. {});
+  lib = pkgs.lib;
+  colconPkgs = lib.filterAttrs (n: v: builtins.substring 0 7 n == "colcon-") pkgs.python3Packages;
+in
+   lib.attrNames colconPkgs' \
+    | jq  --raw-output '.[]' \
+    | while read pkg; do nix-update "$@" python3Packages."$pkg"; done
