833c9a829154349c8c42a30f6e034e0c72769eeb
Ben Wolsieffer <benwolsieffer@gmail.com>
2024-10-14 21:45:27 -0400

    jazzy: urdfdom-headers: update patch

--- distros/jazzy/overrides.nix
+++ distros/jazzy/overrides.nix
@@ -226,8 +226,8 @@ in {
       # Fix CMake relative install dir assumptions
       # https://github.com/ros/urdfdom_headers/pull/66
       (self.fetchpatch {
-        url = "https://github.com/ros/urdfdom_headers/commit/6e0cea148c3a7123f8367cd48d5709a4490c32f1.patch";
-        hash = "sha256-LC2TACGma/k6+WE9fTkzY98SgJYKsVuj5O9v84Q5mQ4=";
+        url = "https://github.com/ros/urdfdom_headers/commit/fa89f2d4744839827f41579004537c966a097681.patch";
+        hash = "sha256-w6PPKCpbR4dGsudVEz+SO9ylXVayLPRAl3VvpMt4DHo=";
       })
     ];
   });
