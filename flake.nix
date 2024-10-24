b23f83fb17861f69202da9eef29c9bdcf7584585
Michal Sojka <michal.sojka@cvut.cz>
2024-10-24 08:43:12 +0200

    flake.nix: Add aach64-linux to checks

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
       };
       checks = {
         x86_64-linux = import ./release.nix { system = "x86_64-linux"; };
+        aarch64-linux = import ./release.nix { system = "aarch64-linux"; };
       };
     };
 
