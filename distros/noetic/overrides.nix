bdd292b3c6cc0666e4af0ac14b038e8924a2a98e
Ben Wolsieffer <benwolsieffer@gmail.com>
2025-03-01 12:30:26 -0500

    noetic: tf: fix Boost compatibility

--- distros/noetic/overrides.nix
+++ distros/noetic/overrides.nix
@@ -168,6 +168,17 @@ in {
     '';
   });
 
+  tf = rosSuper.tf.overrideAttrs ({
+    postPatch ? "", ...
+  }: {
+    # Boost.Math 1.87 requires C++14
+    postPatch = postPatch + ''
+      substituteInPlace CMakeLists.txt \
+        --replace-fail COMPILER_SUPPORTS_CXX11 COMPILER_SUPPORTS_CXX14 \
+        --replace-fail '-std=c++11' '-std=c++14'
+    '';
+  });
+
   tf2 = rosSuper.tf2.overrideAttrs ({
     patches ? [], ...
   }: {
