361dbea1ddbf9ee0f196ce968fe8ee33fbec7294
Ben Wolsieffer <benwolsieffer@gmail.com>
2025-03-01 12:44:06 -0500

    noetic: robot-state-publisher: use C++14

--- distros/noetic/overrides.nix
+++ distros/noetic/overrides.nix
@@ -128,6 +128,17 @@ in {
     sha256 = "sha256-IBlmph3IJvGxh5okozF6HskhSpGMjrA1vi8ww+nPvcs=";
   };
 
+  robot-state-publisher = rosSuper.robot-state-publisher.overrideAttrs ({
+    patches ? [], ...
+  }: {
+    # Use C++14 for Boost.Math compatibility
+    # https://github.com/ros/robot_state_publisher/pull/190
+    patches = patches ++ [ (self.fetchpatch {
+      url = "https://github.com/ros/robot_state_publisher/commit/8478b2aface9e4349f8a0bce61eb74dbfd9bfb63.patch";
+      hash = "sha256-BQt6HoOBMM5bJCADve19uH1RSLLNQlG9aR6g1p58WPw=";
+    }) ];
+  });
+
   rosconsole = rosSuper.rosconsole.overrideAttrs ({
     patches ? [], ...
   }: {
