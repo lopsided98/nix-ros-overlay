9e7a47524c0eb75497845cfd8c31f9ddc16da35e
Superflore <superflore@github.com>
2025-03-01 18:33:54 +0000

    regenerate all distros, Sat Mar  1 18:33:54 2025

--- distros/rolling/image-publisher/default.nix
+++ distros/rolling/image-publisher/default.nix
@@ -5,12 +5,12 @@
 { lib, buildRosPackage, fetchurl, ament-cmake-auto, ament-lint-auto, ament-lint-common, camera-info-manager, cv-bridge, image-transport, rcl-interfaces, rclcpp, rclcpp-components }:
 buildRosPackage {
   pname = "ros-rolling-image-publisher";
-  version = "6.0.9-r1";
+  version = "6.0.10-r1";
 
   src = fetchurl {
-    url = "https://github.com/ros2-gbp/image_pipeline-release/archive/release/rolling/image_publisher/6.0.9-1.tar.gz";
-    name = "6.0.9-1.tar.gz";
-    sha256 = "87eecbd3c6ec3e79ddf81e0857ed1963b09f921782bb69d1b6c2e7a08473033e";
+    url = "https://github.com/ros2-gbp/image_pipeline-release/archive/release/rolling/image_publisher/6.0.10-1.tar.gz";
+    name = "6.0.10-1.tar.gz";
+    sha256 = "f0453d79c9547a1df80ec5104d2516f6290c8032e840084e99aa3b05a310294f";
   };
 
   buildType = "ament_cmake";
