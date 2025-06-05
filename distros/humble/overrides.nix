dad0791c709f9ebb62dbe4d66788048e3b41b45d
Bernd Müller <github@muellerbernd.de>
2025-06-05 06:56:35 +0200

    zenoh-cpp-vendor: Fix build by adding overrides
    Co-authored-by: Michal Sojka <michal.sojka@cvut.cz>

--- distros/humble/overrides.nix
+++ distros/humble/overrides.nix
@@ -310,4 +310,32 @@ in with lib; {
       })
     ];
   });
+
+  zenoh-cpp-vendor = (lib.patchAmentVendorGit rosSuper.zenoh-cpp-vendor { }).overrideAttrs({
+    nativeBuildInputs ? [], postPatch ? "", ...
+  }: let
+      vendoredSourceJson = "${dirOf rosSuper.zenoh-cpp-vendor.meta.position}/vendored-source.json";
+      sourceInfos = builtins.fromJSON (builtins.readFile vendoredSourceJson);
+      zenoh-c-source = self.fetchFromGitHub {
+        owner = "eclipse-zenoh";
+        repo = "zenoh-c";
+        rev = sourceInfos.zenoh_c_vendor.rev;
+        hash = sourceInfos.zenoh_c_vendor.hash;
+      };
+    in {
+    postPatch = postPatch + ''
+      ln -s ${zenoh-c-source.outPath}/Cargo.lock Cargo.lock
+    '';
+    nativeBuildInputs = nativeBuildInputs ++ [
+      self.rustPlatform.cargoSetupHook
+      self.cargo
+      self.rustc
+    ];
+    cargoDeps = self.rustPlatform.importCargoLock {
+      lockFile = "${zenoh-c-source.outPath}/Cargo.lock";
+      outputHashes = {
+        "zenoh-1.4.0" = "sha256-eMrsoeToI8vsRLduRgSFh/yoGt/hHcCAqCGNE6ml9uw=";
+      };
+    };
+  });
 }
