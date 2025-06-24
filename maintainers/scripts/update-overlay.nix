68a02581f585af76a7f94c7a4518d8922c2e0ae4
Michal Sojka <michal.sojka@cvut.cz>
2025-06-24 22:26:20 +0200

    workflows: Make GitHub use the newly added update-overlay "script"

--- maintainers/scripts/update-overlay.nix
+++ maintainers/scripts/update-overlay.nix
@@ -21,8 +21,13 @@ writeShellApplication {
   text = ''
     if [[ $# -eq 0 ]]; then
       echo "No superflore parameters specified, using defaults."
+      if [[ ''${GITHUB_RUNNER_TEMP:-} ]]; then
+        set -- --dry-run --output-repository-path . --all \
+               --tar-archive-dir "$GITHUB_RUNNER_TEMP/tar" --upstream-branch develop
+      else
         set -- --dry-run --output-repository-path . --all \
                --tar-archive-dir .tar --no-branch
+      fi
     fi
     set -x
     mkdir -p "$ROSDEP_SOURCE_PATH"
