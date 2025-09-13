d2280c1397b47c684eea62b05fa7fe5489ac72d2
Michal Sojka <michal.sojka@cvut.cz>
2025-09-14 00:13:27 +0200

    Switch nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ROS overlay for the Nix package manager";
 
   inputs = {
-    nixpkgs.url = "github:lopsided98/nixpkgs/nix-ros";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
