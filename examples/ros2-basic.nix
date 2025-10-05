86fda7a3cab64e91012151a4395107ef5052a334
Michal Sojka <michal.sojka@cvut.cz>
2025-10-05 20:42:50 +0200

    examples/ros2-basic.nix: Make it more flexible
    This adds the following:
    - shell completion
    - possibility to select ROS distro via --argstr rosDistro rolling
    - possibility to use different entry point than ros-core (this will be
      used in next commits)
    
    Compared to previous version, we remove geometry-msgs. Hopefully,
    nobody used this example for real work.

--- examples/ros2-basic.nix
+++ examples/ros2-basic.nix
@@ -1,17 +1,28 @@
 # Environment containing basic ROS2 tools
-
-{ pkgs ? import ../. {} }:
-with pkgs;
-with rosPackages.humble;
-
-mkShell {
-  nativeBuildInputs = [
-    (buildEnv {
+{
+  pkgs ? import ../. { },
+  rosDistro ? "jazzy",
+  package ? "ros-core",
+}:
+pkgs.mkShell {
+  packages = [
+    (pkgs.rosPackages.${rosDistro}.buildEnv {
       paths = [
-        ros-core
-        colcon
-        geometry-msgs
-      ];
+        pkgs.colcon
+      ]
+      ++ (with pkgs.rosPackages.${rosDistro}; [
+        ament-cmake-core
+        python-cmake-module
+
+        pkgs.rosPackages.${rosDistro}.${package}
+      ]);
     })
   ];
+  shellHook = ''
+    # Setup ROS 2 shell completion. Doing it in direnv is useless.
+    if [[ ! $DIRENV_IN_ENVRC ]]; then
+        eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete ros2)"
+        eval "$(${pkgs.python3Packages.argcomplete}/bin/register-python-argcomplete colcon)"
+    fi
+  '';
 }
