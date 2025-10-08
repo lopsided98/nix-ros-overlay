893859b23e55c27422cfccc4d802e6bbe9b46236
Michal Sojka <michal.sojka@cvut.cz>
2025-10-09 00:21:22 +0200

    Remove Gazebo Classic
    Gazebo Classic is EOL since January 2025.

--- pkgs/ignition/fuel-tools/4.nix
+++
@@ -1,9 +0,0 @@
-{ callPackage, ignition, ... } @ args :
-
-callPackage ./. ({
-  majorVersion = "4";
-  version = "4.8.3";
-  srcHash = "sha256-Fa/xKb5J37OM0p8HB+Iu1cA47BbPBYZkO4z8XhSB5oc=";
-  ignition-common = ignition.common3;
-  ignition-msgs = ignition.msgs5;
-} // args)
--- pkgs/ignition/math/4.nix
+++
@@ -1,8 +0,0 @@
-{ callPackage, ignition, ... } @ args :
-
-callPackage ./. ({
-  majorVersion = "4";
-  version = "4.0.0";
-  srcHash = "sha256-GDydOD/n8Ip6cVCBsLhgzweCgRVQjJCwVqIbV1lvfKM=";
-  ignition-cmake = ignition.cmake0;
-} // args)
