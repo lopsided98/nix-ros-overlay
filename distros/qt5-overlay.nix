c7f76bf72439f24efea2bb58cf8196961fb167ae
Michal Sojka <michal.sojka@cvut.cz>
2026-02-15 07:46:27 +0100

    Introduce Qt5 overlay
    Since Humble, Jazzy and Kilted are based on Qt5 and Rolling/Lyrical
    is/will be based on Qt6, we need to use different overrides for these
    two sets of distros. To make it simpler, we merge all qt5-related
    overrides to a single file and apply it to the appropriate
    distrobutions.

---
+++ distros/qt5-overlay.nix
@@ -0,0 +1,6 @@
+# Common overlay for ROS distros using Qt5. At time of writing Humble, Jazzy, Kilted.
+self:
+rosSelf: rosSuper: with rosSelf.lib; {
+  # keep-sorted start block=yes
+  # keep-sorted end
+}
