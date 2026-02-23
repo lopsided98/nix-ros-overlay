ddd52786359f5ddedc54a9b2dd5d0fbab0ba0c8d
Michal Sojka <michal.sojka@cvut.cz>
2026-02-23 22:30:18 +0100

    flake.nix: Switch to unmodified nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ROS overlay for the Nix package manager";
 
   inputs = {
-    nixpkgs.url = "github:lopsided98/nixpkgs/nix-ros";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
