cb442eb2b13aa59ac45a02d666886bdd4d9e94aa
Michal Sojka <michal.sojka@cvut.cz>
2026-03-22 19:10:18 +0100

    jazzy.nav2-behavior-tree: Ignore -Werror-caused build failures with GCC 15

--- distros/jazzy/overrides.nix
+++ distros/jazzy/overrides.nix
@@ -365,7 +365,12 @@ in {
   nav2-behavior-tree = rosSuper.nav2-behavior-tree.overrideAttrs({
     ...
   }: {
-    NIX_CFLAGS_COMPILE = toString [ "-Wno-error=array-bounds" ];
+    NIX_CFLAGS_COMPILE = toString [
+      "-Wno-error=array-bounds"
+      "-Wno-error=maybe-uninitialized"
+      "-Wno-error=free-nonheap-object"
+      "-Wno-error=null-dereference"
+    ];
   });
 
   nav2-costmap-2d = rosSuper.nav2-costmap-2d.overrideAttrs({
