bb3a573d8cb80662b1ec2e93f1f221c2d64f1279
Michal Sojka <michal.sojka@cvut.cz>
2026-05-16 08:46:14 +0200

    Introduce Qt6 overlay
    This will play the same role as qt5-overlay.nix, i.e., injecting the
    correct Qt version dependencies, but for Lyrical and Rolling distros.

---
+++ distros/qt6-overlay.nix
@@ -0,0 +1,7 @@
+# Common overlay for ROS distros using Qt6. At time of writing Lyrical, Rolling
+self:
+rosSelf: rosSuper: with rosSelf.lib; {
+  # keep-sorted start block=yes
+
+  # keep-sorted end
+}
