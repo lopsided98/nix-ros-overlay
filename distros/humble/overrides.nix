0a3959592c89adf5063b526058415d53a88a5a1b
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-19 19:02:00 +0200

    humble.warehouse-ros-sqlite: fix boost system

--- distros/humble/overrides.nix
+++ distros/humble/overrides.nix
@@ -1228,6 +1228,15 @@ in with lib; {
     '';
   });
 
+  warehouse-ros-sqlite = rosSuper.warehouse-ros-sqlite.overrideAttrs ({
+    postPatch ? "", ...
+  }: {
+    # https://github.com/moveit/warehouse_ros_sqlite/pull/61
+    postPatch = postPatch + ''
+      substituteInPlace CMakeLists.txt --replace-fail " system" ""
+    '';
+  });
+
   webots-ros2-driver = rosSuper.webots-ros2-driver.overrideAttrs ({
     postPatch ? "", ...
   }: {
