dad31ad30d35cf5786f3741c1159cc7e01206d89
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-27 22:40:33 +0200

    lyrical.fuse-core: workaround boost range bug

--- distros/lyrical/overrides.nix
+++ distros/lyrical/overrides.nix
@@ -108,7 +108,7 @@ in {
   });
 
   fuse-core = rosSuper.fuse-core.overrideAttrs ({
-    patches ? [], ...
+    patches ? [], postPatch ? "", ...
   }: {
     patches = patches ++ [
       # Don't fail with boost >= 1.86
@@ -119,6 +119,19 @@ in {
         stripLen = 1;
       })
     ];
+
+    # https://github.com/locusrobotics/fuse/pull/424
+    postPatch = postPatch + ''
+      substituteInPlace \
+        include/fuse_core/graph.hpp \
+        include/fuse_core/message_buffer.hpp \
+        include/fuse_core/transaction.hpp \
+        include/fuse_core/timestamp_manager.hpp \
+          --replace-fail \
+            "#include <boost/range/any_range.hpp>" \
+            "#include <boost/type_traits/add_const.hpp>
+             #include <boost/range/any_range.hpp>"
+    '';
   });
 
   gazebo = self.gazebo_11;
