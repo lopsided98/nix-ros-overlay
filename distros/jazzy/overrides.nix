a93197bccfdfdeff4bd5952092f69814ac0617f5
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-27 21:53:15 +0200

    jazzy.motion-capture-tracking: fix boost system

--- distros/jazzy/overrides.nix
+++ distros/jazzy/overrides.nix
@@ -682,6 +682,25 @@ in {
       substituteInPlace deps/libmotioncapture/deps/vrpn/CMakeLists.txt --replace-fail \
         "cmake_minimum_required(VERSION 2.8.3)" \
         "cmake_minimum_required(VERSION 3.10)"
+
+      # https://github.com/IMRCLab/libmotioncapture/pull/33
+      substituteInPlace deps/libmotioncapture/CMakeLists.txt \
+        --replace-fail "Boost 1.5 COMPONENTS system REQUIRED" "Boost REQUIRED" \
+        --replace-fail "Boost::system" "Boost::boost"
+
+      # https://github.com/whoenig/vicon-datastream-sdk/pull/7
+      substituteInPlace deps/libmotioncapture/deps/vicon-datastream-sdk/CMakeLists.txt \
+        --replace-fail \
+          "Boost 1.5 COMPONENTS system thread chrono REQUIRED" \
+          "Boost 1.5 COMPONENTS thread chrono REQUIRED" \
+        --replace-fail "Boost::system" "Boost::boost"
+
+      # https://github.com/whoenig/NatNetSDKCrossplatform/pull/8
+      substituteInPlace deps/libmotioncapture/deps/NatNetSDKCrossplatform/CMakeLists.txt \
+        --replace-fail \
+          "Boost 1.5 REQUIRED COMPONENTS system thread" \
+          "Boost 1.5 REQUIRED COMPONENTS thread" \
+        --replace-fail "Boost::system" "Boost::boost"
     '';
   });
 
