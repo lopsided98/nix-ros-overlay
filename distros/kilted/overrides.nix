ecc02afc16ce2b6355eba72fe899b51f76f2f28a
Michal Sojka <michal.sojka@cvut.cz>
2026-05-30 08:17:00 +0200

    canopen-core: remove unneeded patching

--- distros/kilted/overrides.nix
+++ distros/kilted/overrides.nix
@@ -47,17 +47,6 @@ in {
     ];
   });
 
-  canopen-core = rosSuper.canopen-core.overrideAttrs ({
-    postPatch ? "", ...
-  }: {
-    # https://github.com/ros-industrial/ros2_canopen/pull/399
-    postPatch = ''
-      substituteInPlace ConfigExtras.cmake --replace-fail \
-        "find_package(Boost REQUIRED system thread)" \
-        "find_package(Boost REQUIRED thread)"
-    '';
-  });
-
   cartographer = rosSuper.cartographer.overrideAttrs ({
     postPatch ? "", ...
   }: {
