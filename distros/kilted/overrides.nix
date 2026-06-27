975c0b5416750320b6cfd120b11229e302a09e45
Michal Sojka <michal.sojka@cvut.cz>
2026-06-27 06:51:13 +0200

    warehouse-ros-sqlite: Remove upstreamed fix

--- distros/kilted/overrides.nix
+++ distros/kilted/overrides.nix
@@ -1023,15 +1023,6 @@ in {
     '';
   });
 
-  warehouse-ros-sqlite = rosSuper.warehouse-ros-sqlite.overrideAttrs ({
-    postPatch ? "", ...
-  }: {
-    # https://github.com/moveit/warehouse_ros_sqlite/pull/61
-    postPatch = postPatch + ''
-      substituteInPlace CMakeLists.txt --replace-fail " system" ""
-    '';
-  });
-
   web-video-server = rosSuper.web-video-server.overrideAttrs ({
     postPatch ? "", ...
   }: {
