06862fbbb16509cdfca7de41e69f3afccdabcbe4
Michal Sojka <michal.sojka@cvut.cz>
2026-08-17 23:56:01 +0200

    lyrical.plotjuggler: Don't propagate Qt5
    Plotjuggler doesn't compile with Qt6 yet. Therefore, we need to stick
    with Qt5, but we should not propagate it to avoid Qt version mismatch
    when used in buildEnvs.

--- distros/qt6-overlay.nix
+++ distros/qt6-overlay.nix
@@ -8,6 +8,17 @@ rosSelf: rosSuper: with rosSelf.lib; {
   }: {
     nativeBuildInputs = nativeBuildInputs ++ [ self.qt6.wrapQtAppsHook ];
   });
+  plotjuggler = rosSuper.plotjuggler.overrideAttrs ({
+    buildInputs ? [], propagatedBuildInputs ? [], ...
+  }: {
+    # Plotjuggler needs Qt5 and propagates it. This results in Qt
+    # version mismatch error when plotjuggler is included in a
+    # buildEnv with other Qt6 ROS packages (most of packages in
+    # Lyrical). To avoid that, we do not propagate Qt dependencies
+    # outside of Plotjuggler.
+    propagatedBuildInputs = filter (p: ! hasPrefix "qt" (p.name or "")) propagatedBuildInputs;
+    buildInputs = filter (p: hasPrefix "qt" (p.name or "")) propagatedBuildInputs;
+  });
 
   # keep-sorted end
 }
