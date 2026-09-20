e856675564d2aa8657f5c352a9fe95ed4d233a8c
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-09-20 12:12:17 +0200

    kilted.pick-ik: patch for fmt 11+

--- distros/kilted/overrides.nix
+++ distros/kilted/overrides.nix
@@ -785,6 +785,17 @@ in {
     })
   ];
 
+  pick-ik = rosSuper.pick-ik.overrideAttrs ({
+    postPatch ? "", ...
+  }: {
+    # ref. https://github.com/PickNikRobotics/pick_ik/pull/84 closed
+    postPatch = postPatch + ''
+      substituteInPlace \
+        src/robot.cpp \
+        --replace-fail "<fmt/core.h>" "<fmt/format.h>"
+    '';
+  });
+
   popf = (rosSuper.popf.override {
     # Fix "libfl.so.2: undefined reference to `yylex'"
     flex = self.flex_2_5_35;
